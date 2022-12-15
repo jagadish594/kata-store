247f3b2d35e04ee763f1dde806ebab59c67c1cc3
jagadish mohanram <jagadish594@gmail.com>
2022-12-14 21:25:52 -0800

    added feature for Conjured category

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -11,11 +11,15 @@ class Shop {
     this.items = items;
   }
   updateQuality() {
+
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1;
+          }
+          if (this.items[i].name === 'Conjured Mana Cake') {
+            this.items[i].quality = this.items[i].quality - 2;
           }
         }
       } else {
--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -4,6 +4,14 @@ describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("foo");
+  });
+
+  it("should check for Conjured - item", function() {
+    const gildedRose = new Shop([new Item("Conjured Mana Cake", 3, 6)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("Conjured Mana Cake");
+    expect(items[0].sellIn).toBe(2);
+    expect(items[0].quality).toBe(3);
   });
 });
